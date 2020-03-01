00c84f64c40324026e1515328db38bc0ff0a88ad
unknown <ionita.catalin2000@gmail.com>
2020-03-01 18:24:02 +0200

    am reparat fereastra pentru upload,de asemeena acum poti da click

--- scripts/upload_content.js
+++ scripts/upload_content.js
@@ -4,6 +4,7 @@ let modal = document.querySelector('.upload-modal');
 let uploadButton = document.querySelector('.btn-upload');
 let closeModalButton = document.querySelector('.modal-btn-close');
 let mainContainer = document.querySelector('.grid-wrapper');
+let dragArea=document.querySelector('.file-drag-area');
 
 let isOpened=false;
 
@@ -15,10 +16,14 @@ function closeModal()
 {
     backdrop.style.display='none';
     modal.style.display='none';
+   
 }
 
 uploadButton.onclick=openModal;
 closeModalButton.onclick=closeModal;
+dragArea.onclick=function(){
+    document.getElementById('file-selector').click();
+}
 
 
 
