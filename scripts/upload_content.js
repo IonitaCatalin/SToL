3d88d9a31ac5e52c24407221772ece44ba5eabc5
unknown <ionita.catalin2000@gmail.com>
2020-03-01 16:43:35 +0200

    am adaugat un form pentru operatia de incarcare de fisiere

---
+++ scripts/upload_content.js
@@ -0,0 +1,24 @@
+
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.upload-modal');
+let uploadButton = document.querySelector('.btn-upload');
+let closeModalButton = document.querySelector('.modal-btn-close');
+let mainContainer = document.querySelector('.grid-wrapper');
+
+let isOpened=false;
+
+function openModal(){
+    backdrop.style.display = 'block'; 
+    modal.style.display = 'grid'
+}
+function closeModal()
+{
+    backdrop.style.display='none';
+    modal.style.display='none';
+}
+
+uploadButton.onclick=openModal;
+closeModalButton.onclick=closeModal;
+
+
+
