14b393e6e28e61ec6ddc1da03db8b9b94521450c
unknown <ionita.catalin2000@gmail.com>
2020-03-05 01:10:51 +0200

    prototip de preview la upload prin drag-n-drop

--- scripts/upload_content.js
+++ scripts/upload_content.js
@@ -2,27 +2,67 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.upload-modal');
 let uploadButton = document.querySelector('.btn-upload');
-let closeModalButton = document.querySelector('.modal-btn-close');
+let closeModalButton = document.querySelector('#modal-btn-close');
 let mainContainer = document.querySelector('.grid-wrapper');
-let dragArea=document.querySelector('.file-drag-area');
+let dropArea=document.querySelector('#modal-file-drop');
 
 let isOpened=false;
 
-function openModal(){
+uploadButton.onclick=function(){
     backdrop.style.display = 'block'; 
     modal.style.display = 'grid'
 }
-function closeModal()
-{
+closeModalButton.onclick=function(){
     backdrop.style.display='none';
-    modal.style.display='none';   
+    modal.style.display='none';
+}
+// dragArea.onclick=function(){
+//     document.getElementById('file-selector').click();
+// }
+
+
+dropArea.addEventListener('dragenter',preventDefaults,false);
+dropArea.addEventListener('dragover',preventDefaults,false);
+dropArea.addEventListener('dragleave',preventDefaults,false);
+dropArea.addEventListener('drop',preventDefaults,false);
+
+function preventDefaults(e)
+{
+    e.preventDefault();
+    e.stopPropagation();
 }
 
-uploadButton.onclick=openModal;
-closeModalButton.onclick=closeModal;
-dragArea.onclick=function(){
-    document.getElementById('file-selector').click();
+dropArea.addEventListener('drop',handleDrop,false);
+
+function handleDrop(e)
+{
+    let dt=e.dataTransfer;
+    let files=dt.files;
+    handleFiles(files);
 }
+
+function handleFiles(files)
+{
+    files=[...files];
+    files.forEach(previewFile);
+}
+
+function previewFile(file)
+{
+    let reader=new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend=function(){
+        let img=document.createElement('img');
+        img.src=reader.result;
+        document.getElementById('modal-file-drop').appendChild(img);
+        img.width=50;
+        img.height=50;
+        
+    }
+}
+
+
+
 
 
 
